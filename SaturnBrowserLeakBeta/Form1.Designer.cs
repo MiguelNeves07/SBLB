3e713d33bf39cb0fbaf6a7e7b9addac28e4246ca
neves07 <109999413+MiguelNeves07@users.noreply.github.com>
2026-07-19 21:02:42 -0300

    Add files via upload

---
+++ SaturnBrowserLeakBeta/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace SaturnBrowserLeakBeta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnAvancar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnOficial = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnIr);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(896, 56);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(100, 23);
+            this.btnVoltar.TabIndex = 1;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click_1);
+            // 
+            // btnAvancar
+            // 
+            this.btnAvancar.Location = new System.Drawing.Point(896, 85);
+            this.btnAvancar.Name = "btnAvancar";
+            this.btnAvancar.Size = new System.Drawing.Size(100, 23);
+            this.btnAvancar.TabIndex = 2;
+            this.btnAvancar.Text = "Avançar";
+            this.btnAvancar.UseVisualStyleBackColor = true;
+            this.btnAvancar.Click += new System.EventHandler(this.btnAvancar_Click_1);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(896, 114);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(100, 23);
+            this.btnAtualizar.TabIndex = 3;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click_1);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 15);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(878, 20);
+            this.txtUrl.TabIndex = 4;
+            // 
+            // btnIr
+            // 
+            this.btnIr.Location = new System.Drawing.Point(896, 15);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(100, 20);
+            this.btnIr.TabIndex = 5;
+            this.btnIr.Text = "Ir";
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click_1);
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(12, 56);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(878, 661);
+            this.webView.TabIndex = 6;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(896, 143);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(100, 23);
+            this.btnHome.TabIndex = 7;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnOficial
+            // 
+            this.btnOficial.Location = new System.Drawing.Point(896, 665);
+            this.btnOficial.Name = "btnOficial";
+            this.btnOficial.Size = new System.Drawing.Size(100, 23);
+            this.btnOficial.TabIndex = 8;
+            this.btnOficial.Text = "Site Oficial";
+            this.btnOficial.UseVisualStyleBackColor = true;
+            this.btnOficial.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAbout.Location = new System.Drawing.Point(896, 694);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(100, 23);
+            this.btnAbout.TabIndex = 9;
+            this.btnAbout.Text = "Sobre";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnOficial);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnAvancar);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnAvancar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnIr;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnOficial;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ SaturnBrowserLeakBeta/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace SaturnBrowserLeakBeta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "SaturnBrowserLeakBeta"; // Título do Navegador
+            InicializarNavegador();
+        }
+
+        private async void InicializarNavegador()
+        {
+            // Inicializa o motor Chromium do Edge
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.Navigate("https://www.google.com");
+
+            // Atualiza a barra de endereço ao navegar
+            webView.SourceChanged += (s, e) => {
+                txtUrl.Text = webView.Source.ToString();
+            };
+        }
+
+        // Função do Botão "Ir" (Conforme o erro btnIr_Click_1)
+        private void btnIr_Click_1(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            if (!url.StartsWith("http")) url = "https://" + url;
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(url);
+            }
+        }
+
+        // Função do Botão "Voltar" (Conforme o erro btnVoltar_Click_1)
+        private void btnVoltar_Click_1(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2.CanGoBack)
+            {
+                webView.CoreWebView2.GoBack();
+            }
+        }
+
+        // Função do Botão "Avançar" (Conforme o erro btnAvancar_Click_1)
+        private void btnAvancar_Click_1(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2.CanGoForward)
+            {
+                webView.CoreWebView2.GoForward();
+            }
+        }
+
+        // Função do Botão "Atualizar" (Conforme o erro btnAtualizar_Click_1)
+        private void btnAtualizar_Click_1(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Reload();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Pode deixar vazio, roda ao iniciar o formulário
+        }
+
+        // NOVO: Botão Sobre
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            string infoProjeto = "SaturnBrowserLeakBeta\n" +
+                                 "Versão: 6.7 (Leak)\n\n" +
+                                 "Este navegador não tem nenhuma afiliação com o navegador original Saturn Browser, sendo apenas feito como piada (e com IA).";
+
+            MessageBox.Show(infoProjeto, "Sobre o SaturnBrowserLeakBeta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // NOVO: Vamos usar o "button1" antigo para ser o botão do site oficial (Saturn App)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate("https://saturnbrowser.app");
+            }
+        }
+
+        // NOVO: Botão Home (Leva para o Google)
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate("https://www.google.com");
+            }
+        }
+    }
+}
